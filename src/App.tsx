53a6f64f5fe9b90c674d66b33dc5dd5821fc4634
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-27 18:33:13 -0500

    React + TypeScript: useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Counter } from './components/Counter';
 import { Usuario } from './components/Usuario';
 import { TimerPadre } from './components/TimerPadre';
+import { ContadorRed } from './components/ContadorRed';
 
 function App() {
 	return (
@@ -11,10 +12,11 @@ function App() {
 			<Counter />
 			<Usuario />
 			<hr />
-
 			<h2>useEffect - useRef</h2>
-
 			<TimerPadre />
+			<hr />
+			<h2>useReducer</h2>
+			<ContadorRed />
 		</>
 	);
 }
