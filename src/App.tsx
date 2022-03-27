ef31e997949f9d94c3d248c05e99a76ece33b392
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-27 14:36:14 -0500

    React + TypeScript: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Counter } from './components/Counter';
+import { Usuario } from './components/Usuario';
 function App() {
 	return (
 		<>
 			<h1>React + TypeScript</h1>
+			<hr />
+			<Counter />
+			<Usuario />
 		</>
 	);
 }
